529aeb15f2fa4f09fd5658357595753c6fca1e5a
IB_Student <student@InnovateBhams-MacBook-Pro.local>
2020-02-18 16:50:35 -0600

    up to step 10

--- src/Componants/App.jsx
+++ src/Componants/App.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App(props) {
-    return <h1> Hello {props.name}</h1>
-};
+class App extends Component {
+    state = {
+        text: 'some string',
+        hasLoaded: false
+    }
+    handleChange = (e) => {
+        this.setState({ text: e.target.value })
+    }
+    handleClick = () => this.setState({hasLoaded: true})
+
+    render() {
+        if (this.hasLoaded === true) {
+            return (
+                <React.Fragment>
+                    <h1>Hello {this.props.name} you are supposed to talk about {this.state.text}</h1>
+                    <input type="text" placeholder={this.state.text} onChange={this.handleChange} />
+                </React.Fragment>
+            )
+        } else {
+            return (
+                <React.Fragment>
+                    <h1> Loading ...</h1>
+                    <button onClick={this.handleClick}>Start</button>
+                </React.Fragment>
+            )
+        }
+    }
+}
+
+
+
+
+// function App(props) {
+//     return <h1> Hello {props.name}</h1>
+// };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../src/Componants/App';
 
-ReactDOM.render(<App name = 'Jim' />, document.getElementById('root'));
+ReactDOM.render(<App name = 'Jill' />, document.getElementById('root'));
